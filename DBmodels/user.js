170f6158b4c5e5f43f02e8e24c3b4e3daa932219
Wen <jbw5872@psu.edu>
2022-11-10 02:27:00 -0500

    Implemented user model & Sign-in API to server js

--- DBmodels/user.js
+++ DBmodels/user.js
@@ -1,3 +1,4 @@
+//Credit: Arjun K S, Medium (https://medium.com/swlh/building-a-simple-web-application-with-node-express-mongodb-dcd53231e83c)
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
@@ -5,3 +6,5 @@ const userSchema = mongoose.Schema({
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true }
 });
+
+module.exports = mongoose.model('User',userSchema);
--- server.js
+++ server.js
@@ -6,13 +6,41 @@ var path = require('path');
 var fs = require("fs");
 const mongoose = require('mongoose');
 
+//Express setup ---------------------------------------
 var app = express();
-
 app.use(express.static('public'));
 
-//Mongo DB ---------------------------------------
+//Body parser setup -----------------------------------
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-//Mongo DB ---------------------------------------
+//User model ------------------------------------------
+//Credit: Arjun K S, Medium (https://medium.com/swlh/building-a-simple-web-application-with-node-express-mongodb-dcd53231e83c)
+const User = require('./DBmodels/user.js');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+router.post('/user', (request, response) => {
+const user = new User({
+    userName : request.body.userName,
+    email : request.body.email,
+    password : request.body.password
+});
+bcrypt.hash(user.password, 10, function (err, hash){
+    if (err) {
+        return next(err);
+    }
+    user.password = hash;
+    user.save().then(data => {
+        console.log('New user successfully created');
+    }).catch(error => {
+        // you can send an error code here
+    })
+})
+});
+module.exports = router;
+
+//Mongo DB --------------------------------------------
+
 
 //Connection test
 mongoose.connect('mongodb+srv://gamexhibi:ihatemtx@sweng465-gamexhibi.ijodktv.mongodb.net/?retryWrites=true&w=majority', (err) => {
