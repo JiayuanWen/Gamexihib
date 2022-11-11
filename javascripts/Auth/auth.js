922f925e044fb80693804c3abd1f4a3e8ca59d67
Wen <jbw5872@psu.edu>
2022-11-11 06:52:58 -0500

    Implemented login

--- javascripts/Auth/auth.js
+++ javascripts/Auth/auth.js
@@ -24,3 +24,32 @@ exports.register = async (req, res, next) => {
         })
     }
 };
+
+//Login
+exports.login = async (req, res, next) => {
+    const {username, password} = req.body;
+    if (!username || !password) {
+        return res.status(400).json({
+            message: "Username or Password field is blank"
+        })
+    }
+    try {
+        const user = await User.findOne({username, password})
+        if (!user) {
+            res.status(401).json({
+                message: "Login unsuccessful",
+                error: "User does not exist"
+            })
+        } else {
+            res.status(200).json({
+                message: "Login successful",
+                user
+            })
+        }
+    } catch (err) {
+        res.status(400).json({
+            message: "An error occured",
+            error: err.message
+        })
+    }
+};
--- javascripts/Auth/route.js
+++ javascripts/Auth/route.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const {register} = require('./auth.js');
+
+const {register,login} = require('./auth.js');
 
 router.route('/register').post(register);
+router.route('/login').post(login);
 
 module.exports = router;
