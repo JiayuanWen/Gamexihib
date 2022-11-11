4827e8b5c62cea73bf54a479e29625dde126cf51
Wen <jbw5872@psu.edu>
2022-11-11 00:20:17 -0500

    adjusted user schema

--- javascripts/DBmodels/user.js
+++ javascripts/DBmodels/user.js
@@ -1,10 +1,27 @@
-//Credit: Arjun K S, Medium (https://medium.com/swlh/building-a-simple-web-application-with-node-express-mongodb-dcd53231e83c)
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    userName: { type: String, unique: true,required: true },
-    email: { type: String, unique: true, required: true },
-    password: { type: String, required: true }
+    userName: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 5
+    },
+    role: {
+        type: String,
+        default: "Visitor", //Roles: Moderator, Admin, Visitor
+        required: true
+    }
 });
 
-module.exports = mongoose.model('User',userSchema);
+const User = mongoose.model("user", userSchema);
+module.exports = User;
