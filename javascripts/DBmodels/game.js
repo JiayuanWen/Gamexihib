c5a857a70335c74c8d19d52aa6c9856e51ebbde8
Wen <jbw5872@psu.edu>
2022-11-16 14:26:17 -0500

    Added basic game schema & Game genre object

--- javascripts/DBmodels/game.js
+++ javascripts/DBmodels/game.js
@@ -1,7 +1,26 @@
 const mongoose = require('mongoose');
 
 const gameSchema = mongoose.Schema({
-
+    title: {
+        type: String,
+        required: true
+    },
+    genre: [{
+        type: String,
+        required: true
+    }],
+    description: {
+        type: String,
+        required: true
+    },
+    ageRating: {
+        type: Number,
+        required: true
+    },
+    mtx: {
+       type: Boolean,
+        required: true
+    }
 });
 
 const Game = mongoose.model("game", gameSchema);
---
+++ javascripts/Objects/genres.js
@@ -0,0 +1,24 @@
+const genere = [
+
+    // Action
+    'Platform', //0
+    'Shooter', //1
+    'Fighting', //2
+    'Beat \'em up', //3
+    'Stealth', //4
+    'Survival', //5
+    'Rhythm', //6
+    'Battle Royale', //7
+
+    // Action-adventure
+    'Survival horror', //8
+    'Metroidvania', //9
+    'Text adventure', //10
+    'Graphic adventure', //11
+    'Visual novel', //12
+    'Interactive movie', //13
+    'Real-time 3D adventures' //14
+
+    // Puzzle
+    'Breakout clone', //15
+    ];
