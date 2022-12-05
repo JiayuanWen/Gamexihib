a42a1de7ef98c3f66d8d487c057dd10eff8bcfd6
JiayuanWen <jbw5872@psu.edu>
2022-12-05 08:32:08 -0500

    Added passport logout functionality

--- javascripts/Auth/route.js
+++ javascripts/Auth/route.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 //User
-const {register,login,update,deleteUser} = require('./auth.js');
+const {register,login,logout,update,deleteUser} = require('./auth.js');
     router.route('/register').post(register);
     router.route('/login').post(login);
+    router.route('/logout').post(logout);
     router.route('/updateAcc').put(update);
     router.route('/deleteAcc').delete(deleteUser);
 
